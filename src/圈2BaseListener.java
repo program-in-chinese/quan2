6b46332e2230d78a20c6d42f4a51909cde81e739
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-11-26 13:33:03 -0800

    第一步, 通过Antlr语法文件生成语法词法分析器

---
+++ src/圈2BaseListener.java
@@ -0,0 +1,98 @@
+// Generated from 圈2.g4 by ANTLR 4.7
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link 圈2Listener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class 圈2BaseListener implements 圈2Listener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProgram(圈2Parser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProgram(圈2Parser.ProgramContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterStatement(圈2Parser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitStatement(圈2Parser.StatementContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssign(圈2Parser.AssignContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssign(圈2Parser.AssignContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterPrint(圈2Parser.PrintContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitPrint(圈2Parser.PrintContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAdd(圈2Parser.AddContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAdd(圈2Parser.AddContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ src/圈2Listener.java
@@ -0,0 +1,59 @@
+// Generated from 圈2.g4 by ANTLR 4.7
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link 圈2Parser}.
+ */
+public interface 圈2Listener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link 圈2Parser#program}.
+	 * @param ctx the parse tree
+	 */
+	void enterProgram(圈2Parser.ProgramContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link 圈2Parser#program}.
+	 * @param ctx the parse tree
+	 */
+	void exitProgram(圈2Parser.ProgramContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link 圈2Parser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void enterStatement(圈2Parser.StatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link 圈2Parser#statement}.
+	 * @param ctx the parse tree
+	 */
+	void exitStatement(圈2Parser.StatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link 圈2Parser#assign}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssign(圈2Parser.AssignContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link 圈2Parser#assign}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssign(圈2Parser.AssignContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link 圈2Parser#print}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrint(圈2Parser.PrintContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link 圈2Parser#print}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrint(圈2Parser.PrintContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link 圈2Parser#add}.
+	 * @param ctx the parse tree
+	 */
+	void enterAdd(圈2Parser.AddContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link 圈2Parser#add}.
+	 * @param ctx the parse tree
+	 */
+	void exitAdd(圈2Parser.AddContext ctx);
+}
